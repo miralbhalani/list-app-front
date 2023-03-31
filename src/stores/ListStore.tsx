d88f5f29e0e371211e8fe0f49e1d2044490677eb
miralbhalani2 <miralbhalani2@gmail.com>
2023-03-31 15:15:03 -0700

    comment added

--- src/stores/ListStore.tsx
+++ src/stores/ListStore.tsx
@@ -3,6 +3,7 @@ import { List, ListItem } from '../types';
 
 class ListStore {
 
+    //TODO: A DUMMY OBJECT INCASE IF THE TESTER DONT HAVE BACKEND RUNNING
     lists: List[] = [{
         _id: "123",
         title: "ABC",
